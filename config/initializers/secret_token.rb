708622a6674e1692ed81e93cbb8d51f08d47e406
Steve Long <steven989@gmail.com>
2015-04-20 10:56:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chowdy::Application.config.secret_key_base = 'e169b55dc5e309e09920223cf2c19ef1e609b2cdc2a1e6da360284798bbfe42f1825a4acaced23336579ea8bb86eb16367272db3b8eb5f98946933bd4abf0d9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chowdy::Application.config.session_store :cookie_store, key: '_chowdy_session'
