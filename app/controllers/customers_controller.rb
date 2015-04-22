76e2942b6b3997be5f5944b4a788a1e51e653567
Steve Long <steven989@gmail.com>
2015-04-22 13:01:14 -0400

    automatically match referral and apply discount on Stripe

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -42,21 +42,67 @@ protect_from_forgery :except => :create
             )
 
         #add logic to split odd grean meal numbers
+        #determine gender https://gender-api.com/
+        #auto generate a unique customer ID (that's not a sequential number-based ID)
+        #logic to split meal count into Mondays and Thursdays
+        #add an additional column to track Monday vs. Thursday hubs
 
         #2) system to update the trial end date in stripe using the StartDate model
 
         stripe_customer = Stripe::Customer.retrieve(customer_id)
         stripe_subscription = stripe_customer.subscriptions.retrieve(subscription_id)
-        stripe_subscription.trial_end = StartDate.first.start_date.to_time.to_i
+        stripe_subscription.trial_end = (StartDate.first.start_date+7.days).to_time.to_i
         stripe_subscription.prorate = false
         stripe_subscription.save
 
-        #3) check for potential duplicate payment and send report
+        #3) check for referral and try to match up referrals
 
-        #4) send confirmation email. Add a column to indicate that email has been sent
+        unless referral.blank?
+            referral_match = Customer.where("name ilike ?", referral.downcase)
+            if referral_match.length == 0
+                #send report for manual check
+            elsif referral_match.length == 1
+                #referrer discount
+                stripe_referral_match = Stripe::Customer.retrieve(referral_match.take.stripe_customer_id)
+                stripe_referral_subscription_match = stripe_referral_match.subscriptions.retrieve(referral_match.take.stripe_subscription_id)
+                stripe_referral_subscription_match.coupon = "referral bonus"
+                stripe_referral_subscription_match.prorate = false
+                stripe_referral_subscription_match.save                
+                #referree discount
+                stripe_subscription.coupon = "referral bonus"
+                stripe_subscription.prorate = false
+                stripe_subscription.save
+            elsif referral_match.length > 1
+                #send report for manual check
+            end
+        end
 
-        #5) Any manual overrides required (referral, green meal count can't be parsed)
 
+        #4) check for potential duplicate payment; automatically try to refund based on information
+
+        #5) send confirmation email. Add a column to indicate that email has been sent
+
+        #6) Send report with actions required
+            #unmatched referrals
+            #green meal count can't be parsed
+            #duplicate payment that's not auto refunded
+            #Delivery required --> auto send delivery information request email
+            #Hub not determined
+            #email matches an existing customer
+
+    end
+
+    def show
+        #meal count
+        #hub
+        #regular vs. green, monday vs. thursday split
+        #meal preferences
+        #next billing date
+        #credit card info
+        #email
+        #name
+        #active status
+        #delivery information
     end
 
     # def stripe_update #change customer account information through Stripe webhook
@@ -68,17 +114,32 @@ protect_from_forgery :except => :create
     def update #change customer account information from website
         #affect stripe
             #meal count (change plan in Stripe)      
+            #credit card
+            #email (change in Stripe)
             #pause (change trial date)
             #active/inactive (add/delete subscriptions; new subscriptions to have trial end date associated with it)
         #all database fields
+            #hub (Monday vs. Thursday)
+            #name
+            #meal split between Monday, Thursdan, regular, green; regular preference
+            #delivery
+
+    end
+
+    def customer_sheet
+        #create customer sheets 
     end
 
     def fail #failed charges
-        #generate report (use charge.failed hook)
+        #generate report (use charge.failed hook) for admin
+        #auto email user
     end
 
     def stripe_destroy #delete customer account through Stripe webhook
         #delete on system and Stripe
+    end
+
+    def admin
     end
 
 end
