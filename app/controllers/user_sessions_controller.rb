496b157d7abdea1459b48bfd87b8ada18be6b64b
Steve Long <steven989@gmail.com>
2015-05-13 20:00:49 -0400

    login and sign up screens formatted

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,13 +7,12 @@ class UserSessionsController < ApplicationController
     if @user = login(params[:email], params[:password])
       redirect_to(user_profile_path, notice: 'Login successful')
     else
-      flash.now[:alert] = 'Login failed'
-      render action: 'new'
+      redirect_to(login_path, notice: 'Login failed')
     end
   end
 
   def destroy
     logout
-    redirect_to(login_path, notice: 'Logged out!')
+    redirect_to(login_path, notice: 'Logged out')
   end
 end
