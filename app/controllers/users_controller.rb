f56b959e758fc782f8d65b8a915162f6d4f7b012
Steve Long <steven989@gmail.com>
2015-05-16 18:20:21 -0400

    refinement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     
+    before_filter :require_login, only: :profile
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -11,7 +13,6 @@ class UsersController < ApplicationController
     end
 
     def profile
-        require_login
 
         @current_customer = current_user.customer
         @display_cancel = true
