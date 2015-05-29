f7a0214db830b7dbc712a21a0bb7c6a43b1ed191
Steve Long <steven989@gmail.com>
2015-05-29 18:06:14 -0400

    force perform email delivery in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Chowdy::Application.configure do
   config.assets.version = '1.0'
 
   # Default host for Action Mailer
-  config.action_mailer.default_url_options = {host: 'http://chowdy.ca'}
+  config.action_mailer.default_url_options = {host: 'http://chowdy.herokuapp.com'}
 
 
   # Specifies the header that your server uses for sending files.
@@ -68,6 +68,8 @@ Chowdy::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
