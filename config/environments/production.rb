50fcc02559818da2f9348fb483b9c3fa2872ac51
Steve Long <steven989@gmail.com>
2015-05-29 10:49:23 -0400

    changes for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Chowdy::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -67,7 +67,7 @@ Chowdy::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
