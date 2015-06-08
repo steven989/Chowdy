bd87a5e025863fd57e2228f0e6201ba0b07cc8a3
Steve Long <steven989@gmail.com>
2015-06-08 14:47:38 -0400

    updated asset version so heroku will recompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Chowdy::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Default host for Action Mailer
   config.action_mailer.default_url_options = {host: 'http://chowdy.herokuapp.com'}
