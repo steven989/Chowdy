97058561c2887010840e1249c45c07eeff5bbf14
Steve Long <steven989@gmail.com>
2015-06-11 17:20:51 -0400

    fixed a facebook login bug

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,40 +8,35 @@ class OauthsController < ApplicationController
     stripe_customer_id = params[:state]
     provider = auth_params[:provider]
 
-    if @user = login_from(provider)
+    if @user = login_from(provider) #check if already signed up using facebook, if so then log in
       redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
-    else
+    else #if the user didn't sign up with facebook
       begin
         facebook_email = @user_hash[:user_info]["email"].downcase
-        
         if User.where(email:facebook_email).length == 1 #if someone created an account without facebook, loggin in using Facebook still works
             @user = User.where(email:facebook_email).take
 
             auto_login(@user)
             redirect_to user_profile_path
-        else
-          if Customer.where(email:facebook_email).length > 0
-            flash[:login_error] = "Please use the link in your confirmation email to create an account. If you did not receive the confirmation email, <a href='#{resend_sign_up_link_customer_request_path+"?email="+facebook_email}'>click here</a> to get the link sent to your email again"
-            redirect_to login_path
-          else
-              stripe_customer_id = params[:state]
-              if 
-                if stripe_customer_id.blank?
-                    flash[:login_error] = "Cannot find your subscription. You must <a href='http://chowdy.ca/signup'>sign up</a> for a subscription first to create an account"
-                    redirect_to login_path
-                else
-                    email = Customer.where(stripe_customer_id:stripe_customer_id).take.email 
-                    @user = create_from(provider)
-                    @user.update_attributes(stripe_customer_id: params[:state], email: email)
-                    # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
-
-                    reset_session # protect from session fixation attack
-                    auto_login(@user)
-                    redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
-                end
-
+        else #no user match
+          stripe_customer_id = params[:state]
+          if stripe_customer_id.blank? #if on the login page
+            if Customer.where(email:facebook_email).length > 0
+              flash[:login_error] = "Please use the link in your confirmation email to create an account. If you did not receive the confirmation email, <a href='#{resend_sign_up_link_customer_request_path+"?email="+facebook_email}'>click here</a> to get the link sent to your email again"
+              redirect_to login_path
+            else
+              flash[:login_error] = "Cannot find your subscription. You must <a href='http://chowdy.ca/signup'>sign up</a> for a subscription first to create an account"
+              redirect_to login_path
             end
+          else #if on the sign up page
+              email = Customer.where(stripe_customer_id:stripe_customer_id).take.email 
+              @user = create_from(provider)
+              @user.update_attributes(stripe_customer_id: params[:state], email: email)
+              # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
 
+              reset_session # protect from session fixation attack
+              auto_login(@user)
+              redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
           end
         end
       rescue => error
