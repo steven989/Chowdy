93dd606622686381d87f9bba9900a5ed4d2b234b
Steve Long <steven989@gmail.com>
2015-06-10 20:26:50 -0400

    update

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -7,23 +7,32 @@ class OauthsController < ApplicationController
   def callback
     stripe_customer_id = params[:state]
     provider = auth_params[:provider]
+    puts '---------------------------------------------------'
+    puts '---------------------------------------------------'
+    puts params.inspect
+    puts '---------------------------------------------------'
+    puts auth_params.inspect
+    puts '---------------------------------------------------'
+    puts '---------------------------------------------------'
     if @user = login_from(provider)
       redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
     else
       begin
         stripe_customer_id = params[:state]
-        if stripe_customer_id.blank?
-            flash[:login_error] = "Cannot find your subscription. You must <a href='http://chowdy.ca/signup'>sign up</a> for a subscription to create an account."
-            redirect_to login_path
-        else
-            email = Customer.where(stripe_customer_id:stripe_customer_id).take.email 
-            @user = create_from(provider)
-            @user.update_attributes(stripe_customer_id: params[:state], email: email)
-            # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
+        if 
+          if stripe_customer_id.blank?
+              flash[:login_error] = "Cannot find your subscription. You must <a href='http://chowdy.ca/signup'>sign up</a> for a subscription to create an account."
+              redirect_to login_path
+          else
+              email = Customer.where(stripe_customer_id:stripe_customer_id).take.email 
+              @user = create_from(provider)
+              @user.update_attributes(stripe_customer_id: params[:state], email: email)
+              # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
 
-            reset_session # protect from session fixation attack
-            auto_login(@user)
-            redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
+              reset_session # protect from session fixation attack
+              auto_login(@user)
+              redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
+          end
         end
       rescue => error
         flash[:login_error] = "An error has occurred."
