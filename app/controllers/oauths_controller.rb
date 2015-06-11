0e14891bab7149a9f1326f0a4df83f8de6c2301a
Steve Long <steven989@gmail.com>
2015-06-11 16:47:11 -0400

    updated the login flow

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -9,30 +9,38 @@ class OauthsController < ApplicationController
     provider = auth_params[:provider]
 
     if @user = login_from(provider)
-      puts '---------------------------------------------------'
-      puts '---------------------------------------------------'
-      puts 'this is after login'
-      puts '---------------------------------------------------'
-      puts @user_hash.inspect
-      puts '---------------------------------------------------'
-      puts '---------------------------------------------------'
       redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
     else
       begin
-        stripe_customer_id = params[:state]
-        if 
-          if stripe_customer_id.blank?
-              flash[:login_error] = "Cannot find your subscription. You must <a href='http://chowdy.ca/signup'>sign up</a> for a subscription to create an account."
-              redirect_to login_path
-          else
-              email = Customer.where(stripe_customer_id:stripe_customer_id).take.email 
-              @user = create_from(provider)
-              @user.update_attributes(stripe_customer_id: params[:state], email: email)
-              # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
+        facebook_email = @user_hash[:user_info][:email].downcase
 
-              reset_session # protect from session fixation attack
-              auto_login(@user)
-              redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
+        if User.where(email:facebook_email).length == 1 #if someone created an account without facebook, loggin in using Facebook still works
+            @user = User.where(email:facebook_email).take
+            auto_login(@user)
+            redirect_to user_profile_path
+        else
+          if Customer.where(email:facebook_email).length > 0
+            flash[:login_error] = "Please use the link in your confirmation email to create an account. If you did not receive the confirmation email, <a href='#{resend_sign_up_link_customer_request_path+"?email="+facebook_email}'>click here</a> to get the link sent to your email again"
+            redirect_to login_path
+          else
+              stripe_customer_id = params[:state]
+              if 
+                if stripe_customer_id.blank?
+                    flash[:login_error] = "Cannot find your subscription. You must <a href='http://chowdy.ca/signup'>sign up</a> for a subscription first to create an account"
+                    redirect_to login_path
+                else
+                    email = Customer.where(stripe_customer_id:stripe_customer_id).take.email 
+                    @user = create_from(provider)
+                    @user.update_attributes(stripe_customer_id: params[:state], email: email)
+                    # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
+
+                    reset_session # protect from session fixation attack
+                    auto_login(@user)
+                    redirect_to user_profile_path, :notice => "Logged in from #{provider.titleize}!"
+                end
+
+            end
+
           end
         end
       rescue => error
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,15 +9,15 @@ class UserSessionsController < ApplicationController
     else
       if User.where(email:params[:email].downcase).length == 1
         if User.where(email:params[:email].downcase).take.facebook_email.blank?
-          flash[:login_error] = "Incorrect email or password entered. Please try again."
+          flash[:login_error] = "Incorrect password. Please try again"
         else 
           flash[:login_error] = "Please log in with Facebook"
         end
       else
         if Customer.where(email:params[:email].downcase).length > 0
-          flash[:login_error] = "Please use the link in your confirmation email to create an account. If you did not receive the confirmation email, <a href='#{resend_sign_up_link_customer_request_path+"?email="+params[:email].downcase}'>click here</a> to get the link sent to your email again."
+          flash[:login_error] = "Please use the link in your confirmation email to create an account. If you did not receive the confirmation email, <a href='#{resend_sign_up_link_customer_request_path+"?email="+params[:email].downcase}'>click here</a> to get the link sent to your email again"
         else
-          flash[:login_error] = "Incorrect email or password entered. Please try again."
+          flash[:login_error] = "Cannot find your subscription. You must <a href='http://chowdy.ca/signup'>sign up</a> for a subscription first to create an account"
         end
       end
       redirect_to login_path
