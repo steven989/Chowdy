c7e36962131e4dfaca4f17d80b870f05736aaa04
Steve Long <steven989@gmail.com>
2015-06-16 14:39:38 -0400

    flash for promotion and system setting added

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -16,14 +16,32 @@ class PromotionsController < ApplicationController
     amount = params[:promotion][:amount_in_cents]
 
     if (Promotion.all.map {|p| p.code}.include? code) || (amount.blank?) || (code.blank?) || (Customer.all.map {|c| c.referral_code}.include? code)
+        flash[:status] = "fail"
+        flash[:notice_promotions] = "Promotion cannot be creaetd. Either code or amount is missing, or code already exisits. It could also be matching an existing customer's referral code."
+
         redirect_to user_profile_path+"#promotions"
     else 
+      begin
         stripe_coupon = Stripe::Coupon.create(amount_off:amount, duration:"once", currency:"CAD",id: params[:promotion][:code])
         if stripe_coupon
             promotion.save
             promotion.update_attribute(:stripe_coupon_id, stripe_coupon.id)
         end
-
+      rescue => error
+        flash[:status] = "fail"
+        flash[:notice_promotions] = "Error occurred while creating the promotional coupon on Stripe: #{error.message}"
+        puts '---------------------------------------------------'
+        puts "Error occurred while creating the promotional coupon on Stripe: #{error.message}"
+        puts '---------------------------------------------------'
+      else
+        if promotion.errors.any?
+          flash[:status] = "fail"
+          flash[:notice_promotions] = "Promotion could not be created: #{promotion.errors.full_messages.join(", ")}"
+        else
+          flash[:status] = "success"
+          flash[:notice_promotions] = "Promotion created"          
+        end
+      end
         redirect_to user_profile_path+"#promotions"
     end  
   end
@@ -52,6 +70,14 @@ class PromotionsController < ApplicationController
     promotion = Promotion.find(params[:id])
     promotion.update_attributes(promotion_params)
 
+    if promotion.errors.any?
+      flash[:status] = "fail"
+      flash[:notice_promotions] = "Promotion could not be updated: #{promotion.errors.full_messages.join(", ")}"
+    else
+      flash[:status] = "success"
+      flash[:notice_promotions] = "Promotion updated"          
+    end
+
     redirect_to user_profile_path+"#promotions"
   end
 
@@ -59,10 +85,30 @@ class PromotionsController < ApplicationController
     promotion = Promotion.find(params[:id])
     
     if promotion.stripe_coupon_id.blank?
-        promotion.destroy
+        if promotion.destroy
+          flash[:status] = "success"
+          flash[:notice_promotions] = "Promotion deleted"          
+        else
+          flash[:status] = "fail"
+          flash[:notice_promotions] = "Promotion could not be deleted: #{promotion.errors.full_messages.join(", ")}"
+        end
     else
-        if Stripe::Coupon.retrieve(promotion.stripe_coupon_id).delete
-            promotion.destroy
+        begin 
+          Stripe::Coupon.retrieve(promotion.stripe_coupon_id).delete
+        rescue => error
+          flash[:status] = "fail"
+          flash[:notice_promotions] = "Error occurred while deleting the promotional coupon on Stripe: #{error.message}"
+          puts '---------------------------------------------------'
+          puts "Error occurred while deleting the promotional coupon on Stripe: #{error.message}"
+          puts '---------------------------------------------------'
+        else
+          if promotion.destroy
+            flash[:status] = "success"
+            flash[:notice_promotions] = "Promotion deleted"          
+          else
+            flash[:status] = "fail"
+            flash[:notice_promotions] = "Promotion could not be deleted: #{promotion.errors.full_messages.join(", ")}"
+          end
         end
     end
     redirect_to user_profile_path+"#promotions"
--- app/controllers/system_settings_controller.rb
+++ app/controllers/system_settings_controller.rb
@@ -12,7 +12,13 @@ class SystemSettingsController < ApplicationController
 
   def create
     system_setting = SystemSetting.new(system_setting_params)
-    system_setting.save
+    if system_setting.save
+      flash[:status] = "success"
+      flash[:notice_system_settings] = "System setting created"
+    else
+      flash[:status] = "fail"
+      flash[:notice_system_settings] = "System setting could not be created: #{system_setting.errors.full_messages.join(", ")}"
+    end
     redirect_to user_profile_path+"#system_settings"
   end
 
@@ -28,6 +34,15 @@ class SystemSettingsController < ApplicationController
   def update
     system_setting = SystemSetting.find(params[:id])
     system_setting.update_attributes(system_setting_params)
+
+    if system_setting.errors.any?
+      flash[:status] = "fail"
+      flash[:notice_system_settings] = "System setting could not be update: #{system_setting.errors.full_messages.join(", ")}"
+    else
+      flash[:status] = "success"
+      flash[:notice_system_settings] = "System setting updated"
+    end
+
     redirect_to user_profile_path+"#system_settings"
   end
 
@@ -45,13 +60,27 @@ class SystemSettingsController < ApplicationController
   def create_announcement
     @scope = params[:system_setting][:setting_attribute].blank? ? "all" : params[:system_setting][:setting_attribute]
     @system_setting = SystemSetting.new(setting:"announcement", setting_attribute:@scope, setting_value: params[:system_setting][:setting_value])
-    @system_setting.save
+    if @system_setting.save
+      flash[:status] = "success"
+      flash[:notice_system_settings] = "Announcement created for #{@scope} customers"
+    else
+      flash[:status] = "fail"
+      flash[:notice_system_settings] = "Announcement could not be created: #{@system_setting.errors.full_messages.join(", ")}"
+    end
+
     redirect_to user_profile_path+"#system_settings"
   end
 
   def destroy
     system_setting = SystemSetting.find(params[:id])
-    system_setting.destroy
+    if system_setting.destroy
+      flash[:status] = "success"
+      flash[:notice_system_settings] = "System setting deleted"
+    else
+      flash[:status] = "fail"
+      flash[:notice_system_settings] = "System setting could not be deleted: #{system_setting.errors.full_messages.join(", ")}"
+    end
+
     redirect_to user_profile_path+"#system_settings"
   end
 
