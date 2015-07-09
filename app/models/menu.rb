4701cf365ce243fca0a8c251d1e9aaabd5a49f62
Steve Long <steven989@gmail.com>
2015-07-09 12:00:35 -0400

    updated average rating rounding

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu < ActiveRecord::Base
 
     def refresh_rating
         number_of_ratings = self.menu_ratings.length
-        average_rating = self.menu_ratings.sum(:rating).to_f/self.menu_ratings.length.to_f
+        average_rating = ((self.menu_ratings.sum(:rating).to_f/self.menu_ratings.length.to_f) * 100).round / 100.0
         self.update_attributes(average_score:average_rating,number_of_scores:number_of_ratings)        
     end
 
