dc78219cd3d91f4f9462bf0bcbb401b2634bac5f
Steve Long <steven989@gmail.com>
2015-09-13 00:42:57 -0400

    updated code for daily snapshot

--- app/models/daily_snapshot.rb
+++ app/models/daily_snapshot.rb
@@ -4,7 +4,7 @@ class DailySnapshot < ActiveRecord::Base
     def self.take_snapshot
         current_pick_up_date = SystemSetting.where(setting:"system_date", setting_attribute:"pick_up_date").take.setting_value.to_date
         active_nonpaused_customers = Customer.where(active?: ["Yes","yes"], paused?: [nil,"No","no"], next_pick_up_date:current_pick_up_date)
-        active_customers = Customer.where(active?: ["Yes","yes"], next_pick_up_date:current_pick_up_date)
+        active_customers = Customer.where(active?: ["Yes","yes"])
 
         existing_date = DailySnapshot.retrieve_snapshot(Date.today)
         if existing_date
