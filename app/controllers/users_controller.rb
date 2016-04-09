7715288e19a26c139e0e310cde9c8646dafcd54a
Steve Long <steven989@gmail.com>
2016-04-09 14:42:01 -0400

    fixed an error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -216,7 +216,7 @@ class UsersController < ApplicationController
             current_user.log_activity("chef dashboard")
             @menu = Menu.all.order(production_day: :asc)
         else
-            @display_marketplace_to_customers = @current_user.email == "tiffany.bayliss@gmail.com" true : false #testing account
+            @display_marketplace_to_customers = @current_user.email == "tiffany.bayliss@gmail.com" ? true : false #testing account
             @current_customer = current_user.customer
             @marketplace_delivery_date = PartnerProductDeliveryDate.first.delivery_date.strftime("%A %B %e, %Y")
             @display_cancel = true
