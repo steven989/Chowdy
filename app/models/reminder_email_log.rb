2a38d0432270dee2fea98d3633c50882ce63803f
Steve Long <steven989@gmail.com>
2016-06-07 20:48:18 -0400

    wip

--- app/models/reminder_email_log.rb
+++ app/models/reminder_email_log.rb
@@ -1,3 +1,39 @@
 class ReminderEmailLog < ActiveRecord::Base
     belongs_to :customer, primary_key: :stripe_customer_id, foreign_key: :stripe_customer_id
+
+    def self.generate_customer_list(duration)
+        # not currently active
+        Customer.where(active?:["No","no",nil,""])
+        Customer.find_by_sql(
+            "
+
+            Select 
+                *
+
+            From customers a
+            Right Join 
+
+                    (
+                    Select
+                        a.id as customer_id,
+                        max(b.created_at) as latest_cancel_request_created_at,
+                        max(c.created_at) as latest_reminder_email_sent,
+
+                        
+
+                    From customers a
+                    Left Join stop_requests b on a.stripe_customer_id = b.stripe_customer_id and b.request_type ilike '%cancel%' and b.cancel_reason ilike '%taking%break%'
+                    Left Join reminder_email_logs c on a.stripe_customer_id = c.stripe_customer_id
+                    Left Join no_email_customers d on a.stripe_customer_id = d.stripe_customer_id
+                    Where (a.active? in ('No',no,'') or a.active? is NULL) and (c.stripe_customer_id is NULL)
+                    Group by a.id
+                    ) b on a.id = b.customer_id
+            Where 
+                (b.latest_reminder_email_sent is NULL or ((b.latest_reminder_email_sent < b.latest_cancel_request_created_at) AND (b.latest_reminder_email_sent < ( current_date + (-65 * interval '1 day'))) ))
+                AND b.latest_cancel_request_created_at < ( current_date + (-65 * interval '1 day'))
+
+            ")
+
+    end
+
 end
