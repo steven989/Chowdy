3c2aab228362c7b5f663a31518bfe601fae0d678
Steve Long <steven989@gmail.com>
2016-06-14 10:47:11 -0400

    fixed error

--- app/models/reminder_email_log.rb
+++ app/models/reminder_email_log.rb
@@ -34,9 +34,9 @@ class ReminderEmailLog < ActiveRecord::Base
             discount = c.reminder_email_logs.blank? ? 500 : nil
             c.add_discount_to_stripe(discount,"discount for restarting Chowdy subscription") if discount
             ReminderEmailLog.create(stripe_customer_id:c.stripe_customer_id,date_reminder_sent:Date.today)
-            CustomerMailer.delay.restart_reminder(customer)
+            CustomerMailer.restart_reminder(c).deliver
             if c.user
-                c.user.log_activity("System: created and sent restart email #{discount ? 'with discount of $'+(discount.to_f/100).round(2).to_s : 'wiht no discount'}")
+                c.user.log_activity("System: created and sent restart email #{discount ? 'with discount of $'+(discount.to_f/100).round(2).to_s : 'with no discount'}")
             end
         end
     end
